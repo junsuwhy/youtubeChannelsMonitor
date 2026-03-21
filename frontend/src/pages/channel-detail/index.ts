58daa61255a4d4fd9b071a92fbdc2cd0371d4b89
Chang Shu-Huai <junsuwhy@gmail.com>
2026-03-21 13:20:51 +0800

    [frontend] Add index export for channel-detail tabs

---
+++ frontend/src/pages/channel-detail/index.ts
@@ -0,0 +1,4 @@
+export { VideosTab } from "./VideosTab";
+export { AnomaliesTab } from "./AnomaliesTab";
+export { FetchLogsTab } from "./FetchLogsTab";
+export { SettingsTab } from "./SettingsTab";
